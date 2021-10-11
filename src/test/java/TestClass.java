e4e2dfeecba1bb58ad72dbc35eaeda28314c2f97
Tihomir Minchev <tihomir.minchev@mentormate.com>
2021-10-11 04:35:18 +0300

    Add test to JSON schema validation

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,8 +1,10 @@
 import static common.APIResources.users;
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema;
 import static org.hamcrest.Matchers.hasSize;
 
 import io.restassured.response.Response;
+import java.io.File;
 import org.junit.jupiter.api.Test;
 
 public class TestClass extends TestBase{
@@ -18,5 +20,16 @@ public class TestClass extends TestBase{
                .and()
                     .body("findAll{it.username.equals('Delphine')}", hasSize(1))
                     .extract();
+    }
+
+    @Test()
+    public void validateUsersJsonSchemaUser(){
+        File jsonSchema = new File(System.getProperty("user.dir") + "/src/main/resources/json-schema/get-users-json-schema.json");
+
+        given()
+            .and()
+                .get(users)
+            .then()
+                .body(matchesJsonSchema(jsonSchema));
     }
 }
