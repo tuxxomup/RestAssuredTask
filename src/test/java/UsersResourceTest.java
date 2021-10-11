c934d9d40a9ef2c4cb14bc11bce6b4359d4224f5
Tihomir Minchev <tihomir.minchev@mentormate.com>
2021-10-11 05:01:16 +0300

    Create User models and get as object

--- src/test/java/UsersResourceTest.java
+++ src/test/java/UsersResourceTest.java
@@ -6,6 +6,8 @@ import static org.hamcrest.Matchers.hasSize;
 import common.TestBase;
 import io.restassured.response.Response;
 import java.io.File;
+import java.util.List;
+import models.User;
 import org.junit.jupiter.api.Test;
 
 public class UsersResourceTest extends TestBase {
@@ -26,7 +28,7 @@ public class UsersResourceTest extends TestBase {
 
     @Test()
     public void getUsers() {
-        Response postsResponse =
+        Response response =
             (Response) given()
                 .and()
                     .spec(requestSpec)
@@ -37,5 +39,7 @@ public class UsersResourceTest extends TestBase {
                .and()
                     .body("findAll{it.username.equals('Delphine')}", hasSize(1))
                     .extract();
+
+        List<User> users =  response.body().jsonPath().getList(".", User.class);
     }
 }
