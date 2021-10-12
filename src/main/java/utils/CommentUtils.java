41729bfef79c50af2f83fd03945a0ad6a8aef97a
Tihomir Minchev <tihomir.minchev@mentormate.com>
2021-10-12 10:33:12 +0300

    Optimize the test for email validation

--- src/main/java/utils/CommentUtils.java
+++ src/main/java/utils/CommentUtils.java
@@ -5,8 +5,12 @@ import static common.TestBase.responseOkSpec;
 import static io.restassured.RestAssured.given;
 
 import io.restassured.response.Response;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import models.Comment;
+import models.Post;
+import org.apache.commons.validator.routines.EmailValidator;
 
 public class CommentUtils {
 
@@ -15,5 +19,20 @@ public class CommentUtils {
             given().pathParam("postId", postId).get(postIdComments).then().spec(responseOkSpec).extract().response();
         List<Comment> commentList = response.body().jsonPath().getList(".", Comment.class);
         return commentList;
+    }
+
+    public static List<Comment> getInvalidEmailInPostComments(List<Post> posts){
+
+        List<Comment> allInvalidComments =
+            collectAllCommentsPerPosts(posts).stream().filter(u -> !EmailValidator.getInstance().isValid(u.getEmail())).collect(Collectors.toList());
+        return allInvalidComments;
+    }
+
+    public static List<Comment> collectAllCommentsPerPosts(List<Post> posts){
+        List<Comment> allCommentPerPost = new ArrayList<>();
+        for(Post post: posts){
+            allCommentPerPost.addAll(getCommentsPerPostId(post.getId()));
+        }
+        return allCommentPerPost;
     }
 }
--- src/test/java/PostResourceTest.java
+++ src/test/java/PostResourceTest.java
@@ -2,16 +2,17 @@ import static common.APIResources.posts;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.junit.Assert.assertTrue;
+import static utils.UserUtils.getUserIdByUsername;
 
 import common.Constants;
 import common.TestBase;
 import java.util.List;
 import models.Comment;
 import models.Post;
-import org.apache.commons.validator.routines.EmailValidator;
 import org.junit.Test;
 import utils.CommentUtils;
 import utils.PostUtils;
+
 
 public class PostResourceTest extends TestBase {
 
@@ -32,17 +33,16 @@ public class PostResourceTest extends TestBase {
             .response();
 
         List<Post> userPostList = PostUtils.getPostPerUsername(Constants.delphineUserSearchTerms);
+        List<Comment> commentsWithInvalidEmail = CommentUtils.getInvalidEmailInPostComments(userPostList);
+        logInvalidEmails(commentsWithInvalidEmail, getUserIdByUsername(Constants.delphineUserSearchTerms));
+        assertTrue("There is/are invalid email formats", commentsWithInvalidEmail.isEmpty());
+    }
 
-        for(Post post: userPostList){
-
-            List<Comment> userCommentList = CommentUtils.getCommentsPerPostId(post.getUserId());
-            for(Comment comment: userCommentList){
-                String errorMessage =
-                    String.format("Post ID:%d by user with ID:%d and comment ID:%d has a email:%s, which is wrong!!!",
-                        post.getId(), post.getUserId(), comment.getId(), comment.getEmail());
-
-                assertTrue(errorMessage, EmailValidator.getInstance().isValid(comment.getEmail()));
-            }
+     void logInvalidEmails(List<Comment> invalidEmail, int userId){
+        for (Comment comment: invalidEmail){
+            String logInvalidEmail = String.format("Post ID:%s by user with ID:%d and comment ID:%s has an email:%s, which is wrong!!!",
+                comment.getPostId(), userId, comment.getId(), comment.getEmail());
+           System.out.println(logInvalidEmail);
         }
     }
 }
